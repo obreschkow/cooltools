d34117fd0224876f31007fcb8e11beb52d2a0b5f
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2019-05-03 13:04:14 +0800

    cst

---
+++ R/cst.R
@@ -0,0 +1,23 @@
+#' Scientific constants
+#'
+#' @description The list \code{cst} contains a lot of useful scientific constants in SI units, mainly for astrophysics.
+#'
+#' @author Danail Obreschkow
+#'
+#' @export cst
+
+cst = list(G = 6.67408e-11, # [m^3/kg/s^2] gravitational constant
+           kb = 1.38064852e-23, # [m^2 kg s^(-2) K^(-1)] Boltzmann constant
+           Msun = 1.98847e30, # [kg] solar mass
+           Mearth = 5.972e24, # [kg] Earth mass
+           AU = 149597870700, # [m] astronomical unit
+           yr = 31557600, # [s] Julian astronomical year year
+           month = 2629743.83333, # [s] month = year/12
+           day = 86400, # [s] day
+           hour = 3600, # [s] hour
+           min = 60, # [s] minute
+           pc = 3.0857e16, # [m] parsec
+           kpc = 3.0857e19, # [m] kiloparsec
+           Mpc = 3.0857e22, # [m] Megaparsec
+           Gpc = 3.0857e25, # [m] Gigaparsec
+           ly = 9.4605284) # [m] light-year
