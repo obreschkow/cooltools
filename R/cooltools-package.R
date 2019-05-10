a40ab1f540bd412eff0ec86574c9ecec2e3b89ff
obreschkow <danail.obreschkow@gmail.com>
2019-05-10 23:00:45 +0800

    helpfile

--- R/cooltools-package.R
+++ R/cooltools-package.R
@@ -1,6 +1,6 @@
 #' Routines for scientific research
 #'
-#' This package contains a useful collection of routines for scientific research, gathered through paractical experience.
+#' This package contains a useful collection of routines for scientific research, grown organically through practical experience. These routines span across numerical analysis, graphics, statistics and system functions.
 #'
 #' @docType package
 #' @name cooltools
