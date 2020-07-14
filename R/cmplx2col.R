b578308f71ea6259c6cd469ab0447bd38cedd517
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-07-14 13:57:51 +0800

    fix

--- R/cmplx2col.R
+++ R/cmplx2col.R
@@ -5,7 +5,7 @@
 #' @description Converts a complex number (or a vector/array thereof) into a color-string, such that brightness represents the complex amplitude and hue represents the complex phase.
 #'
 #' @param z complex number or vector/array of complex numbers
-#' @param max maximum
+#' @param max value of the complex module that corresponds to full brightness; if not given, this is set equal to the maximum complex amplitude in z.
 #' @param hue hue value of positive reals
 #' @param saturation saturation value of colors
 #'
@@ -15,9 +15,11 @@
 #'
 #' @export
 
-cmplx2col = function(z, max=1, hue=0, saturation=1) {
+cmplx2col = function(z, max=NULL, hue=0, saturation=1) {
   amp = Mod(z)
   phi = Arg(z)
+  if (is.null(max)) max = max(amp)
+  max = max(.Machine$double.xmin,max)
   col = hsv(((phi/2/pi)+hue)%%1, saturation, pmax(0,pmin(1,amp/max)))
   if (length(dim(z))>1) col = array(col,dim(z))
   return(col)
