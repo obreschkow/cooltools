7759b051a895d635691542706b607e00e61e8f87
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-09-18 23:28:26 +0800

    version 1.24

--- R/colorbar.R
+++ R/colorbar.R
@@ -14,6 +14,8 @@
 #' @param crange 2-vector specifying the range of values, linearly represented by the full color scale
 #' @param clim optional 2-vector specifying a sub-interval of crange to which the color scale is cropped
 #' @param show.border logical flag specifying whether to draw a rectangular border around the bar
+#' @param text axis label
+#' @param line distance between label and color bar
 #' @param show.axis logical flag specifying whether to draw an axis
 #' @param side character string, which specifies the location of the axis on the bar. This has to be 'left' or 'right' (default).
 #' @param lwd linewidth of border and ticks
@@ -37,7 +39,7 @@
 colorbar = function(xleft, ybottom, xright, ytop,
                     col = gray.colors(256,0,1), n = length(col),
                     crange = c(0,1), clim = NULL,
-                    show.border = TRUE,
+                    show.border = TRUE, text = '', line=2,
                     show.axis = TRUE, side = 'right', lwd=1, nticks=5, at=NULL, ...) {
 
   # avoid clipped margin
@@ -63,10 +65,13 @@ colorbar = function(xleft, ybottom, xright, ytop,
 
   # plot axis
   if (show.axis) {
+
+    # redefine plotting area
+    usr = par()$usr
     b = diff(clim)/(ytop-ybottom)
     a = clim[1]-b*ybottom
-    usr = par()$usr
     par(usr=c(usr[1:2],a+b*usr[3:4]))
+
     if (is.null(at)) at=seq(clim[1], clim[2], length=nticks)
     if (side=='left') {
       axis(side=2, pos=xleft, at=at, lwd = NA, lwd.ticks = lwd, ...)
@@ -75,7 +80,18 @@ colorbar = function(xleft, ybottom, xright, ytop,
     } else {
       stop('unknown side')
     }
+
+    # restore plotting area
     par(usr=usr)
+  }
+
+  # label axis
+  if (side=='left') {
+    text(xleft-line*(xright-xleft),(ybottom+ytop)/2,text,srt='90')
+  } else if (side=='right') {
+    text(xright+line*(xright-xleft),(ybottom+ytop)/2,text,srt='90')
+  } else {
+    stop('unknown side')
   }
 
   # plot border
