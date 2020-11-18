6ef4829cced9d9f1b2d1680fb497c661eb6271f1
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-11-18 11:36:55 +0800

    vectorproduct acceleration

--- R/vectorproduct.R
+++ R/vectorproduct.R
@@ -15,6 +15,9 @@
 
 vectorproduct = function(x,y,normalize=FALSE) {
 
+  x = as.matrix(x)
+  y = as.matrix(y)
+
   if (length(x)!=length(y)) stop('x and y must have the same length')
 
   if (length(x)==3) {
@@ -22,12 +25,10 @@ vectorproduct = function(x,y,normalize=FALSE) {
     y = array(y,c(1,3))
   }
 
-  n = dim(x)[1]
-  z = array(NA,c(n,3))
-
-  for (i in seq(n)) {
-    z[i,] = c(x[i,2]*y[i,3]-x[i,3]*y[i,2], x[i,3]*y[i,1]-x[i,1]*y[i,3], x[i,1]*y[i,2]-x[i,2]*y[i,1])
-  }
+  z = array(NA,c(dim(x)[1],3))
+  z[,1] = x[,2]*y[,3]-x[,3]*y[,2]
+  z[,2] = x[,3]*y[,1]-x[,1]*y[,3]
+  z[,3] = x[,1]*y[,2]-x[,2]*y[,1]
 
   if (normalize) z = unitvector(z)
 
