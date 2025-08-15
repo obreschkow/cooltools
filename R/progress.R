9c46c86ea7fff6c76f3c26e2e9d470d412612ed6
obreschkow <danail.obreschkow@gmail.com>
2025-08-15 12:13:10 +0800

    update to progress function

--- R/progress.R
+++ R/progress.R
@@ -1,6 +1,7 @@
 #' Show progress while timer in running
 #'
-#' @description Show progress while a timer is running following a call of tick().
+#' @description Show progress while a timer is running following a call of tick(). To suppress all progress updates,
+#' set the environment variable .cooltools.env$suppressProgress to \code{TRUE}.
 #'
 #' @param txt custom text to be displayed. If not provided, the time since calling tick() is displayed.
 #'
@@ -27,9 +28,11 @@ progress = function(txt=NULL) {
   if (is.null(.cooltools.env$timerRunning) || !.cooltools.env$timerRunning) {
     cat('Warning: progress can only be called if a timer has been started with tick(), but not yet stopped with tock().')
   } else {
-    if (is.null(txt)) txt=sprintf('(%.2fs)',as.double(proc.time()[3]-get("tickTime", envir = .cooltools.env)))
-    cat(sprintf(' %s%s',strrep("\b \b", .cooltools.env$progress_nchar),txt))
-    assign("progress_nchar", nchar(txt)+1, envir = .cooltools.env)
+    if (is.null(.cooltools.env$suppressProgress) || !.cooltools.env$suppressProgress) {
+      if (is.null(txt)) txt=sprintf('(%.2fs)',as.double(proc.time()[3]-get("tickTime", envir = .cooltools.env)))
+      cat(sprintf(' %s%s',strrep("\b \b", .cooltools.env$progress_nchar),txt))
+      assign("progress_nchar", nchar(txt)+1, envir = .cooltools.env)
+    }
   }
 
 }
