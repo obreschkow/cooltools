38c7793f1afc7e0a6b0adaddcd8181bbb71d4176
obreschkow <danail.obreschkow@gmail.com>
2026-06-26 14:15:03 +0800

    updated quadrupole and inertia functions

--- R/inertia.R
+++ R/inertia.R
@@ -1,30 +1,67 @@
-#' Inertia tensor
+#' Moment of inertia tensor
 #'
-#' @description Computes the symmetric tensor of moments of inertia
+#' @description
+#' Computes the moment of inertia tensor of a set of point masses in arbitrary
+#' dimensions,
+#' \deqn{
+#' \mathbf{I}
+#' =
+#' \sum_i m_i
+#' \left(
+#' |\mathbf{x}_i|^2\mathbf{1}
+#' -
+#' \mathbf{x}_i\mathbf{x}_i^\mathrm{T}
+#' \right),
+#' }
+#' where \eqn{\mathbf{1}} is the identity matrix.
 #'
-#' @param x n-by-3 matrix (x[1:n,1:3]) specifying the 3D Cartesian coordinates of n points
-#' @param m n-vector with point masses, or single scalar giving a uniform mass for all points (default is unity)
+#' @param x Numeric vector or matrix. If a matrix is supplied, each row is
+#'   interpreted as a point.
+#' @param m Numeric scalar or vector of point masses. If a scalar is supplied,
+#'   the same mass is assigned to every point.
 #'
-#' @return Returns a 3-by-3 symmetric matrix
+#' @return
+#' A symmetric \eqn{d\times d} matrix containing the moment of inertia tensor,
+#' where \eqn{d} is the dimension of the points.
 #'
-#' @author Danail Obreschkow
-#'
-#' @seealso \code{\link{quadrupole}}, \code{\link{moments}}
+#' @seealso
+#' \code{\link{quadrupole}}, \code{\link{moments}}
 #'
 #' @export
 
-inertia = function(x,m=1) {
+inertia = function(x, m = 1) {
 
-  if (length(x)==3) x = array(x,c(1,3)) # to make sure that x is an array
+  x = as.matrix(x)
+  if (ncol(x) == 1) x = t(x)
 
-  I = array(NA,c(3,3))
-  I[1,1] = sum(m*(x[,2]^2+x[,3]^2))
-  I[2,2] = sum(m*(x[,3]^2+x[,1]^2))
-  I[3,3] = sum(m*(x[,1]^2+x[,2]^2))
-  I[1,2] = I[2,1] = -sum(m*x[,1]*x[,2])
-  I[2,3] = I[3,2] = -sum(m*x[,2]*x[,3])
-  I[3,1] = I[1,3] = -sum(m*x[,3]*x[,1])
+  n = nrow(x)
+  d = ncol(x)
 
-  return(I)
+  if (length(m) == 1)
+    m = rep(m, n)
 
+  if (length(m) != n)
+    stop("m must have length one or the number of rows of x")
+
+  M = crossprod(x, x * m)
+  trM = sum(m * rowSums(x^2))
+
+  trM * diag(d) - M
 }
+
+# PREVIOUS SIMPLE 3D-ONLY VERSION, TESTED TO GIVE IDENTICAL RESULTS IN 3D
+# inertia = function(x,m=1) {
+#
+#   if (length(x)==3) x = array(x,c(1,3)) # to make sure that x is an array
+#
+#   I = array(NA,c(3,3))
+#   I[1,1] = sum(m*(x[,2]^2+x[,3]^2))
+#   I[2,2] = sum(m*(x[,3]^2+x[,1]^2))
+#   I[3,3] = sum(m*(x[,1]^2+x[,2]^2))
+#   I[1,2] = I[2,1] = -sum(m*x[,1]*x[,2])
+#   I[2,3] = I[3,2] = -sum(m*x[,2]*x[,3])
+#   I[3,1] = I[1,3] = -sum(m*x[,3]*x[,1])
+#
+#   return(I)
+#
+# }
--- R/quadrupole.R
+++ R/quadrupole.R
@@ -1,30 +1,70 @@
 #' Quadrupole tensor
 #'
-#' @description Compute the trace-free quadrupole tensor of a set of point masses
+#' @description
+#' Computes the trace-free mass quadrupole tensor of a set of point masses in
+#' arbitrary dimensions,
+#' \deqn{
+#' \mathbf{Q}
+#' =
+#' \sum_i m_i
+#' \left(
+#' d\,\mathbf{x}_i\mathbf{x}_i^\mathrm{T}
+#' -
+#' |\mathbf{x}_i|^2\mathbf{I}
+#' \right),
+#' }
+#' where \eqn{d} is the dimensionality of the points and \eqn{\mathbf{I}} is
+#' the \eqn{d\times d} identity matrix.
 #'
-#' @param x n-by-3 matrix (x[1:n,1:3]) specifying the 3D Cartesian coordinates of n points
-#' @param m n-vector with point masses, or single scalar giving a uniform mass for all points (default is unity)
+#' @param x Numeric vector or matrix. If a matrix is supplied, each row is
+#'   interpreted as a point.
+#' @param m Numeric scalar or vector of point masses. If a scalar is supplied,
+#'   the same mass is assigned to every point.
 #'
-#' @return Returns a 3-by-3 symmetric matrix
-#'
-#' @author Danail Obreschkow
-#'
-#' @seealso \code{\link{inertia}}, \code{\link{moments}}
+#' @return
+#' A symmetric \eqn{d\times d} matrix containing the trace-free quadrupole
+#' tensor.
 #'
 #' @export
+#'
+quadrupole = function(x, m = 1) {
 
-quadrupole = function(x,m=1) {
+  x = as.matrix(x)
+  if (ncol(x) == 1) x = t(x)
 
-  if (length(x)==3) x = array(x,c(1,3)) # to make sure that x is an array
+  n = nrow(x)
+  d = ncol(x)
 
-  Q = array(NA,c(3,3))
-  Q[1,1] = sum(m*(2*x[,1]^2-x[,2]^2-x[,3]^2))
-  Q[2,2] = sum(m*(2*x[,2]^2-x[,3]^2-x[,1]^2))
-  Q[3,3] = sum(m*(2*x[,3]^2-x[,1]^2-x[,2]^2))
-  Q[1,2] = Q[2,1] = sum(m*(3*x[,1]*x[,2]))
-  Q[2,3] = Q[3,2] = sum(m*(3*x[,2]*x[,3]))
-  Q[3,1] = Q[1,3] = sum(m*(3*x[,3]*x[,1]))
+  if (length(m) == 1)
+    m = rep(m, n)
 
-  return(Q)
+  if (length(m) != n)
+    stop("m must have length one or the number of rows of x")
 
+  # sum_i m_i x_i x_i^T
+  M = crossprod(x, x * m)
+
+  # sum_i m_i |x_i|^2
+  r2 = rowSums(x^2)
+  tr = sum(m * r2)
+
+  # trace-free quadrupole tensor
+  d * M - tr * diag(d)
 }
+
+# PREVIOUS SIMPLE 3D-ONLY VERSION, TESTED TO GIVE IDENTICAL RESULTS IN 3D
+# quadrupole2 = function(x,m=1) {
+#
+#   if (length(x)==3) x = array(x,c(1,3)) # to make sure that x is an array
+#
+#   Q = array(NA,c(3,3))
+#   Q[1,1] = sum(m*(2*x[,1]^2-x[,2]^2-x[,3]^2))
+#   Q[2,2] = sum(m*(2*x[,2]^2-x[,3]^2-x[,1]^2))
+#   Q[3,3] = sum(m*(2*x[,3]^2-x[,1]^2-x[,2]^2))
+#   Q[1,2] = Q[2,1] = sum(m*(3*x[,1]*x[,2]))
+#   Q[2,3] = Q[3,2] = sum(m*(3*x[,2]*x[,3]))
+#   Q[3,1] = Q[1,3] = sum(m*(3*x[,3]*x[,1]))
+#
+#   return(Q)
+#
+# }
